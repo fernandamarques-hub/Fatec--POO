abe57b0404ea5f574c550e952cd1f570e6a0c502
Fernanda Marques <56441190+fernandamarques-hub@users.noreply.github.com>
2020-10-06 18:08:25 -0300

    Update Menu.java

--- Lista 01/wb_lista1/src/wb/Menu.java
+++ Lista 01/wb_lista1/src/wb/Menu.java
@@ -3,7 +3,7 @@ package wb;
 public class Menu {
 	public static void cadastroUnidade() {
 		System.out.println("\n*************************");
-		System.out.println("\nSelecione a opção desejada:");
+		System.out.println("\nSelecione a opÃ§Ã£o desejada:");
 		System.out.println("\n1. Cadastrar unidade.");
 		System.out.println("\n2. Listar unidades.");
 		System.out.println("\n3. Entrar na Agenda de uma unidade.");
@@ -13,37 +13,38 @@ public class Menu {
 
 	public static void mostrarMenu() {
 		System.out.println("\n*************************");
-		System.out.println("\nSelecione a opção desejada:");
+		System.out.println("\nSelecione a opÃ§Ã£o desejada:");
 		System.out.println("\n1. Cadastrar cliente.");
 		System.out.println("\n2. Editar cliente.");
 		System.out.println("\n3. Excluir cliente.");
-		System.out.println("\n4. Listar clientes.");
-		System.out.println("\n5. Listar clientes por gênero.");
-		System.out.println("\n6. Listar serviços/produtos consumidos de um cliente.");
-		System.out.println("\n7. Relatórios.");
+		System.out.println("\n4. Adicionar serviÃ§o Ã  um cliente.");
+		System.out.println("\n5. Listar clientes.");
+		System.out.println("\n6. Listar clientes por gÃªnero.");
+		System.out.println("\n7. Listar serviÃ§os/produtos consumidos de um cliente.");
+		System.out.println("\n8. RelatÃ³rios.");
 		System.out.println("\n0. Sair");
 		System.out.println("\n*************************");
 	}
 	
 	public static void mostrarRelatorio() {
 		System.out.println("\n*************************");
-		System.out.println("\nSelecione a opção desejada:");
-		System.out.println("\n1. Idade média de todo o público de uma unidade.");
-		System.out.println("\n2. Idade média do público para um determinado gênero.");
-		System.out.println("\n3. Serviço mais procurado.");
-		System.out.println("\n4. Serviço mais procurado para um determinado gênero.");
+		System.out.println("\nSelecione a opÃ§Ã£o desejada:");
+		System.out.println("\n1. Idade mÃ©dia de todo o pÃºblico de uma unidade.");
+		System.out.println("\n2. Idade mÃ©dia do pÃºblico para um determinado gÃªnero.");
+		System.out.println("\n3. ServiÃ§o mais procurado.");
+		System.out.println("\n4. ServiÃ§o mais procurado para um determinado gÃªnero.");
 		System.out.println("\n0. Voltar");
 		System.out.println("\n*************************");
 	}
 	
 	public static void mostrarEdicao() {
 		System.out.println("\n*************************");
-		System.out.println("\nSelecione a opção desejada:");
+		System.out.println("\nSelecione a opÃ§Ã£o desejada:");
 		System.out.println("\n1. Editar nome.");
 		System.out.println("\n2. Editar telefone.");
 		System.out.println("\n3. Editar data de nascimento.");
-		System.out.println("\n4. Editar gênero.");
-		System.out.println("\n5. Adicionar serviços.");
+		System.out.println("\n4. Editar gÃªnero.");
+		System.out.println("\n5. Adicionar serviÃ§os.");
 		System.out.println("\n0. Sair");
 		System.out.println("\n*************************");
 	}
